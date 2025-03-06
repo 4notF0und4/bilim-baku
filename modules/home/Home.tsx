62e154d386bb9058ead5ff5158768e842c859b61
Asiman Jafarov <157046249+4notF0und4@users.noreply.github.com>
2025-03-06 12:56:44 +0400

    Update Home.tsx

--- modules/home/Home.tsx
+++ modules/home/Home.tsx
@@ -1,30 +1,3 @@
-
-
-// import Header from "@/modules/home/components/Header";
-// import HeaderNews from "@/modules/home/components/header-news/HeaderNews";
-// import styles from "./home.module.css";
-// import CardsGrid from "./components/cards/CardsGrid";
-
-// const Home = () => {
-//   return (
-//     <div>
-//       <div className={styles["home-container"]}>
-//         <div className={styles["content-wrapper"]}>
-//           <Header />
-//           <main className={styles["main-content"]}>
-//             <HeaderNews />
-//           </main>
-//         </div>
-//       </div>
-//       <div className={styles["card-wrapper"]}>
-//         <CardsGrid />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
 import Header from "@/modules/home/components/Header";
 import HeaderNews from "@/modules/home/components/header-news/HeaderNews";
 import styles from "./home.module.css";
